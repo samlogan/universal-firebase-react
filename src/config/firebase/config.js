32a79ddabc817eb21537a768ea9c7f4feb9f337d
Sam Logan <sam@birdbrain.com.au>
2016-07-22 12:44:12 +1000

    Dummy Firebase config added

--- src/config/firebase/config.js
+++ src/config/firebase/config.js
@@ -1,6 +1,6 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  storageBucket: ""
+  apiKey: "AIzaSyBBYhrRs-mrfyB2VM4Tkqiv1sQ6aqEB2JU",
+    authDomain: "react-redux-firebase-d8246.firebaseapp.com",
+    databaseURL: "https://react-redux-firebase-d8246.firebaseio.com",
+    storageBucket: "react-redux-firebase-d8246.appspot.com"
 };
