a6555d0d4fd7f14b92256f7de238944abe00c36c
samlogan <sam@sjlwebdesign.com>
2018-02-22 13:54:16 +1100

    Firebase actions ES7 async await tidy up

--- app/actions/firebase.js
+++ app/actions/firebase.js
@@ -21,40 +21,36 @@ const usersRef = firebaseDb.ref('users');
 
 /* Sign in / Sign off actions
 =================================================== */
-export function signInError(error) {
-  return dispatch => {
-    console.error('Error signing in', error);
-    dispatch({type: SIGN_IN_ERROR, payload: getErrorFromCode(error.code)});
-    setTimeout(() => {
-      dispatch({ type: RESET_AUTH_ERRORS });
-    }, 5000);
-  };
-}
-export function signInSuccess(profile, reroute, redirect) {
-  return dispatch => {
-    const nextPath = !redirect ? `/account/${profile.uid}` : redirect;
-    dispatch({ type: SIGN_IN_SUCCESS, payload: profile});
-    if (reroute) browserHistory.push(nextPath);
-  };
-}
-export function signOutSuccess() {
-  return (dispatch) => {
-    dispatch({type: SIGN_OUT_SUCCESS});
-    dispatch(postAlert('You have successfully signed out', 'success'));
-    browserHistory.push('/');
-  };
-}
-export function signOut(event) {
-  return dispatch => {
+export const signInError = (error) => dispatch => {
+  console.error('Error signing in', error);
+  dispatch({type: SIGN_IN_ERROR, payload: getErrorFromCode(error.code)});
+  setTimeout(() => {
+    dispatch({ type: RESET_AUTH_ERRORS });
+  }, 5000);
+};
+export const signInSuccess = (profile, reroute, redirect) => dispatch => {
+  const nextPath = !redirect ? `/account/${profile.uid}` : redirect;
+  dispatch({ type: SIGN_IN_SUCCESS, payload: profile});
+  if (reroute) browserHistory.push(nextPath);
+};
+export const signOutSuccess = () => (dispatch) => {
+  dispatch({type: SIGN_OUT_SUCCESS});
+  dispatch(postAlert('You have successfully signed out', 'success'));
+  browserHistory.push('/');
+};
+export const signOut = (event) => async dispatch => {
+  try {
     if (event) event.preventDefault();
-    firebaseAuth.signOut()
-      .then(() => dispatch(signOutSuccess()));
-  };
-}
+    await firebaseAuth.signOut();
+    dispatch(signOutSuccess());
+  } catch (error) {
+    console.error('Error signing out', error);
+  }
+};
 /* Provider Signup
 =================================================== */
-export function createProfileFromProvider(result, redirect) {
-  return dispatch => {
+export const createProfileFromProvider = (result, redirect) => async dispatch => {
+  try {
     const userRef = usersRef.child(result.uid);
     const userInfo = {
       uid: result.uid,
@@ -66,53 +62,51 @@ export function createProfileFromProvider(result, redirect) {
       providerType: result.providerData[0].providerId,
       photoURL: result.providerData[0].photoURL
     };
-    userRef.once('value')
-    .then(snapshot => {
-      // Existing user
-      if (snapshot.val()) dispatch(signInSuccess(snapshot.val(), true, redirect));
-      // New User
-      else {
-        userRef.update(userInfo, error => {
-          if (error) {
-            console.error('ERROR @ User :', error);
-          } else {
-            dispatch(signInSuccess(userInfo, true, redirect));
-          }
-        });
-      }
-    });
-  };
-}
-function authenticate(provider, redirect) {
-  return dispatch => {
+    const snapshot = await userRef.once('value');
+    // Existing user
+    if (snapshot.val()) return dispatch(signInSuccess(snapshot.val(), true, redirect));
+    // New User
+    await userRef.update(userInfo);
+    return dispatch(signInSuccess(userInfo, true, redirect));
+  } catch (error) {
+    return console.error('Error creating profile from provider', error);
+  }
+};
+const authenticate = (provider, redirect) => async dispatch => {
+  try {
     dispatch({type: SIGN_IN});
-    firebaseAuth.signInWithPopup(provider)
-    .then(result => dispatch(createProfileFromProvider(result.user, redirect)))
-    .catch(error => dispatch(signInError(error)));
-  };
-}
-export function signInWithGoogle(redirect) {
-  return authenticate(new firebase.auth.GoogleAuthProvider(), redirect);
-}
-export function signInWithTwitter(redirect) {
-  return authenticate(new firebase.auth.TwitterAuthProvider(), redirect);
-}
-export function signInWithGithub(redirect) {
+    const result = await firebaseAuth.signInWithPopup(provider);
+    dispatch(createProfileFromProvider(result.user, redirect));
+  } catch (error) {
+    dispatch(signInError(error));
+  }
+};
+export const signInWithGoogle = (redirect) => authenticate(new firebase.auth.GoogleAuthProvider(), redirect);
+export const signInWithTwitter = (redirect) => authenticate(new firebase.auth.TwitterAuthProvider(), redirect);
+export const signInWithGithub = (redirect) => {
   const provider = new firebase.auth.GithubAuthProvider();
   const scopes = ['user', 'repo', 'delete_repo', 'read:org', 'write:org'];
   scopes.map(scope => provider.addScope(scope));
   return authenticate(provider, redirect);
-}
-export function signInWithFacebook(redirect) {
+};
+export const signInWithFacebook = (redirect) => {
   const provider = new firebase.auth.FacebookAuthProvider();
   const scopes = ['email'];
   scopes.map(scope => provider.addScope(scope));
   return authenticate(provider, redirect);
-}
+};
+/* Fetch user profile
+=================================================== */
+export const fetchProfile = (result, reroute, redirect) => async dispatch => {
+  const userRef = usersRef.child(result.uid);
+  const snapshot = await userRef.once('value');
+  const userInfo = {...snapshot.val(), uid: result.uid};
+  dispatch(signInSuccess(userInfo, reroute, redirect));
+};
 /* Email Signup
 =================================================== */
-export function createProfileFromEmail(result, firstName, lastName) {
-  return dispatch => {
+export const createProfileFromEmail = (result, firstName, lastName) => async dispatch => {
+  try {
     const userRef = usersRef.child(result.uid);
     const userInfo = {
       firstName,
@@ -120,73 +114,53 @@ export function createProfileFromEmail(result, firstName, lastName) {
       email: result.email,
       uid: result.uid
     };
-    userRef.update(userInfo, error => {
-      if (error) {
-        console.error('ERROR @ User :', error);
-      } else {
-        dispatch(signInSuccess(userInfo, true));
-      }
-    });
-  };
-}
-export function createUserWithEmail(props) {
-  const { firstName, lastName, email, password } = props;
-  return (dispatch) => {
+    await userRef.update(userInfo);
+    dispatch(signInSuccess(userInfo, true));
+  } catch (error) {
+    console.error('Error creating profile from email address', error);
+  }
+};
+export const createUserWithEmail = (props) => async (dispatch) => {
+  try {
+    const { firstName, lastName, email, password } = props;
     dispatch({type: SIGN_IN});
-    firebaseAuth.createUserWithEmailAndPassword(email, password)
-    .then(result => dispatch(createProfileFromEmail(result, firstName, lastName)))
-    .catch(error => dispatch(signInError(error)));
-  };
-}
-export function signInWithEmail(props, redirect) {
-  const { email, password } = props;
-  return dispatch => {
+    const result = await firebaseAuth.createUserWithEmailAndPassword(email, password);
+    dispatch(createProfileFromEmail(result, firstName, lastName));
+  } catch (error) {
+    dispatch(signInError(error));
+  }
+};
+export const signInWithEmail = (props, redirect) => async dispatch => {
+  try {
+    const { email, password } = props;
     dispatch({type: SIGN_IN});
-    firebaseAuth.signInWithEmailAndPassword(email, password)
-      .then(result => dispatch(fetchProfile(result, true, redirect)))
-      .catch(error => dispatch(signInError(error)));
-  };
-}
-export function resetPasswordFromEmail(email) {
-  return (dispatch) => {
+    const result = await firebaseAuth.signInWithEmailAndPassword(email, password);
+    dispatch(fetchProfile(result, true, redirect));
+  } catch (error) {
+    dispatch(signInError(error));
+  }
+};
+export const resetPasswordFromEmail = (email) => async dispatch => {
+  try {
     dispatch({type: PASSWORD_RESET});
-    firebaseAuth.sendPasswordResetEmail(email)
-      .then(() => {
-        dispatch(postAlert(`A password reset email has been sent to ${email}`, 'success'));
-        dispatch({type: PASSWORD_RESET_SUCCESS});
-      })
-      .catch(error => {
-        dispatch({type: PASSWORD_RESET_FAILURE, payload: getErrorFromCode(error.code)});
-        setTimeout(() => {
-          dispatch({ type: RESET_AUTH_ERRORS });
-        }, 5000);
-      });
-  };
-}
-/* Check Auth and fetch user profile
+    await firebaseAuth.sendPasswordResetEmail(email);
+    dispatch(postAlert(`A password reset email has been sent to ${email}`, 'success'));
+    dispatch({type: PASSWORD_RESET_SUCCESS});
+  } catch (error) {
+    dispatch({type: PASSWORD_RESET_FAILURE, payload: getErrorFromCode(error.code)});
+    setTimeout(() => {
+      dispatch({ type: RESET_AUTH_ERRORS });
+    }, 5000);
+  }
+};
+/* Check Auth
 =================================================== */
-export function fetchProfile(result, reroute, redirect) {
-  return (dispatch) => {
-    const userRef = usersRef.child(result.uid);
-    userRef.once('value').then(snapshot => {
-      const userInfo = {...snapshot.val(), uid: result.uid};
-      dispatch(signInSuccess(userInfo, reroute, redirect));
-    });
-  };
-}
-export function checkAuth() {
-  return dispatch => {
-    firebaseAuth.onAuthStateChanged(user => {
-      if (user) dispatch(fetchProfile(user, false));
-      else dispatch({ type: INIT_AUTH });
-    });
-  };
-}
+export const checkAuth = () => async dispatch => {
+  const user = await firebaseAuth.onAuthStateChanged();
+  if (user) dispatch(fetchProfile(user, false));
+  else dispatch({ type: INIT_AUTH });
+};
 /* Handle redirecting user post signin
 =================================================== */
-export function captureRedirect(path) {
-  return { type: CAPTURE_REDIRECT, payload: path};
-}
-export function clearRedirect() {
-  return { type: CLEAR_REDIRECT };
-}
+export const captureRedirect = (payload) => ({ type: CAPTURE_REDIRECT, payload });
+export const clearRedirect = () => ({ type: CLEAR_REDIRECT });
