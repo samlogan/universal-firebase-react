097736ff928829fc23c620570fa64e61d0a82c86
samlogan <sam@sjlwebdesign.com>
2018-07-27 12:10:33 +1000

    Fix in posts action

--- app/actions/posts.js
+++ app/actions/posts.js
@@ -25,8 +25,8 @@ const assignPostToUser = (key, props) => async (dispatch) => {
     const user = firebaseAuth.currentUser;
     const userPostsRef = usersRef.child(`${user.uid}/posts`);
     const userPostRef = userPostsRef.child(key);
-    await userPostRef.set({uploaded: +new Date(), lastModified: +new Date()});
-    createPostSuccess(props);
+    await userPostRef.set({ uploaded: +new Date(), lastModified: +new Date() });
+    dispatch(createPostSuccess(props));
   } catch (error) {
     dispatch(createPostError(key, error));
   }
@@ -37,7 +37,12 @@ export const addPost = props => async (dispatch) => {
   dispatch({type: CREATE_POST});
   const postRef = postsRef.push();
   try {
-    const postData = {...props, uploaded: +new Date(), lastModified: +new Date(), uid: postRef.key};
+    const postData = {
+      ...props,
+      uploaded: +new Date(),
+      lastModified: +new Date(),
+      uid: postRef.key
+    };
     await postRef.set(postData);
     dispatch(assignPostToUser(postRef.key, postData));
   } catch (error) {
