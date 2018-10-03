86d533bb68d83bd451a8254efa7e01532783c9e7
samlogan <sam@sjlwebdesign.com>
2018-10-04 09:12:47 +1000

    Added SVG loader, split up images/files in webpack, added compression

---
+++ webpack/rules/file.js
@@ -0,0 +1,10 @@
+const PATHS = require('../paths');
+
+module.exports = ({
+  test: /\.(woff|woff2|ttf|mp4)$/,
+  loader: 'url-loader',
+  options: {
+    name: '[hash].[ext]'
+  },
+  include: PATHS.app
+});
