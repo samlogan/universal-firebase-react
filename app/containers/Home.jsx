6c4af1171b2535b990d2bf333c51c6fa41a271d9
samlogan <sam@sjlwebdesign.com>
2018-10-04 15:20:01 +1000

    Added blur up technique to images

--- app/containers/Home.jsx
+++ app/containers/Home.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Head } from '../components/Common/Head';
 import { Loading } from '../components/Content/Loading';
 import { PostList } from '../components/Home/PostList';
+import LazyImage from '../components/Common/LazyImage';
+import waterfall from '../images/waterfall.jpg';
 
 // eslint-disable-next-line
 class Home extends Component {
@@ -12,6 +14,7 @@ class Home extends Component {
     return (
       <main className="container">
         <Head title="Welcome" />
+        <LazyImage {...waterfall} alt="Hero Image" />
         <div className="wrapper">
           <p>This is home</p>
           <PostList posts={posts} />
